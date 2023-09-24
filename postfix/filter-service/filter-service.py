03f12c86baa5f766b6e1704bf1a3863e5b47d9b5
Gregory Ruta <102509965@student.swin.edu.au>
2023-09-25 09:14:59 +1000

    Fix filter service

--- postfix/filter-service/filter-service.py
+++ postfix/filter-service/filter-service.py
@@ -107,6 +107,9 @@ if re.findall(domainRegex, emailFromAddress)[0] == "internal.test":
             newBody = "Encrypted message attached"
             emailObj.set_content(newBody)
 
+           # Give the email a new subject
+            emailObj["Subject"] = "Encrypted message attached"
+
             # Pipe original email body into gpg and save symetrically
             # encrypted file that will be attached to the email
             encryptedAttachmentPath = f'/home/user/encrypted-{randomChars}.gpg'
